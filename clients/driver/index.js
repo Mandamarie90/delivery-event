324146cd9388765be3f4328e49b13805a6ff42dc
Amanda <amandamarquez27@icloud.com>
2024-05-15 17:12:59 -0700

    added a queue

---
+++ clients/driver/index.js
@@ -0,0 +1,34 @@
+const io = require('socket.io-client');
+const socket = io.connect('http://localhost:3000');
+
+socket.on('connect', () => {
+  console.log('Driver connected');
+  requestNextOrder();
+});
+
+socket.on('pickup', (payload) => {
+  console.log('Driver received pickup notification', payload);
+  if (payload.orderId) {
+    console.log('Driver: picking up', payload.orderId);
+
+    setTimeout(() => {
+      console.log('Driver: in-transit', payload.orderId);
+      socket.emit('package-in-transit', payload);
+    }, 1000);
+
+    setTimeout(() => {
+      console.log('Driver: delivered', payload.orderId);
+      socket.emit('package-delivered', payload);
+      requestNextOrder(); // Request next order after delivering
+    }, 3000);
+  } else {
+    console.error('Invalid payload structure:', payload);
+  }
+});
+
+function requestNextOrder() {
+  console.log('Driver: requesting next order');
+  socket.emit('driver-ready');
+}
+
+module.exports = socket;
--- server/index.js
+++ server/index.js
@@ -1,40 +1,54 @@
-const io = require('socket.io')(3000);
+'use strict';
 
-io.on('connection', (socket) => {
-  console.log('Client connected:', socket.id);
+require('dotenv').config();
 
-  socket.on('pickup', (payload) => {
-    console.log('Event: pickup', payload);
-    socket.broadcast.emit('pickup', payload);  
+const PORT = process.env.PORT || 3000;
+console.log('Listening on port:', PORT);
+const io = require('socket.io')(PORT);
+
+// Save the orders in a queue
+const Queue = require('./lib/queue');
+const orders = new Queue();
+
+io.on('connection', handleClientConnection);
+
+function handleClientConnection(socket) {
+  console.log('New Connection:', socket.id);
+
+  socket.on('driver-ready', () => {
+    console.log('Driver is ready, checking for orders...');
+    let nextOrder = orders.dequeue();
+    if (nextOrder) {
+      console.log('HUB: Sending Order to Driver', nextOrder);
+      console.log('Orders in the queue:', orders.length());
+      console.log('---------------');
+      socket.emit('pickup', nextOrder);
+    } else {
+      console.log('HUB: No Orders in Queue');
+      console.log('---------------');
+    }
   });
 
-  socket.on('in-transit', (payload) => {
-    console.log('Event: in-transit', payload);
-    io.emit('in-transit', payload);  
+  socket.on('ready-for-pickup', (payload) => {
+    payload.status = 'queued for pickup';
+    orders.enqueue(payload);
+
+    console.log('HUB: Order Queued for Pickup', payload);
+    console.log('Orders in the queue:', orders.length());
+    console.log('---------------');
   });
 
-  socket.on('delivered', (payload) => {
-    console.log('Event: delivered', payload);
-    io.emit('delivered', payload); 
+  socket.on('package-in-transit', (payload) => {
+    payload.status = 'in transit';
+    console.log('HUB: Package in Transit', payload);
+    console.log('---------------');
+    io.emit('in-transit', payload);
   });
-});
- 
 
-
-// Socket.on('pickup' order) => {  //I heard you
-//   io.emit('pickup', order);   // Im telling everyone else
-// };
-
-// Socket.on('deliver')
-
-// Socket.on
-
-// hub needs to hear everything and tell everyone
-
-
-// been delivered, 1 console
-//thank you, other console. 
-
-//no namespacing today or rooms
-
-//emit for everyone and and on for everyone
+  socket.on('package-delivered', (payload) => {
+    payload.status = 'delivered';
+    console.log('HUB: Package Delivered', payload);
+    console.log('---------------');
+    io.emit('delivered', payload);
+  });
+}
